d3b30484b18d73ff2109a0c777f8e95b1d830349
bdrupieski <brian.drupieski@gmail.com>
2017-01-19 21:51:58 -0500

    add english word count ratio to score breakdown on stats page

--- db/anagrams.js
+++ db/anagrams.js
@@ -806,7 +806,8 @@ WITH scores_attempted_approval AS (SELECT
                                      avg(anagram_matches.interesting_factor)                             AS interesting_factor,
                                      avg(anagram_matches.inverse_lcs_length_to_total_length_ratio)       AS lcs_ratio,
                                      avg(anagram_matches.different_word_count_to_total_word_count_ratio) AS wc_ratio,
-                                     avg(anagram_matches.edit_distance_to_length_ratio)                  AS ed_ratio
+                                     avg(anagram_matches.edit_distance_to_length_ratio)                  AS ed_ratio,
+                                     avg(anagram_matches.english_words_to_total_word_count_ratio)        AS ewc_ratio
                                    FROM anagram_matches
                                    WHERE
                                      anagram_matches.attempted_approval IS TRUE
@@ -816,7 +817,8 @@ WITH scores_attempted_approval AS (SELECT
                           avg(anagram_matches.interesting_factor)                             AS interesting_factor,
                           avg(anagram_matches.inverse_lcs_length_to_total_length_ratio)       AS lcs_ratio,
                           avg(anagram_matches.different_word_count_to_total_word_count_ratio) AS wc_ratio,
-                          avg(anagram_matches.edit_distance_to_length_ratio)                  AS ed_ratio
+                          avg(anagram_matches.edit_distance_to_length_ratio)                  AS ed_ratio,
+                          avg(anagram_matches.english_words_to_total_word_count_ratio)        AS ewc_ratio
                         FROM anagram_matches
                         WHERE anagram_matches.rejected IS TRUE
                               AND anagram_matches.interesting_factor > $1::float)
@@ -824,7 +826,8 @@ SELECT
   scores_attempted_approval.interesting_factor - scores_rejected.interesting_factor AS if_approved_surplus,
   scores_attempted_approval.lcs_ratio - scores_rejected.lcs_ratio                   AS lcs_approved_surplus,
   scores_attempted_approval.wc_ratio - scores_rejected.wc_ratio                     AS wc_approved_surplus,
-  scores_attempted_approval.ed_ratio - scores_rejected.ed_ratio                     AS ed_approved_surplus
+  scores_attempted_approval.ed_ratio - scores_rejected.ed_ratio                     AS ed_approved_surplus,
+  scores_attempted_approval.ewc_ratio - scores_rejected.ewc_ratio                   AS ewc_approved_surplus
 FROM scores_attempted_approval, scores_rejected;
 `;
 
@@ -835,6 +838,7 @@ FROM scores_attempted_approval, scores_rejected;
             lcsApprovedSurplus: row.lcs_approved_surplus,
             wcApprovedSurplus: row.wc_approved_surplus,
             edApprovedSurplus: row.ed_approved_surplus,
+            ewcApprovedSurplus: row.ewc_approved_surplus,
         };
     });
 };
@@ -860,7 +864,13 @@ WITH score_buckets AS (SELECT DISTINCT ON (interesting_factor)
                          OVER w   AS ed_ratio_attempted,
                          avg(CASE WHEN anagram_matches.rejected IS TRUE
                            THEN anagram_matches.edit_distance_to_length_ratio END)
-                         OVER w   AS ed_ratio_rejected
+                         OVER w   AS ed_ratio_rejected,
+                        avg(CASE WHEN anagram_matches.attempted_approval IS TRUE AND anagram_matches.rejected IS FALSE
+                           THEN anagram_matches.english_words_to_total_word_count_ratio END)
+                         OVER w   AS ewc_ratio_attempted,
+                         avg(CASE WHEN anagram_matches.rejected IS TRUE
+                           THEN anagram_matches.english_words_to_total_word_count_ratio END)
+                         OVER w   AS ewc_ratio_rejected
                        FROM anagram_matches
                        WINDOW w AS (
                          PARTITION BY trunc(anagram_matches.interesting_factor :: NUMERIC, 2)
@@ -869,7 +879,8 @@ SELECT
   interesting_factor,
   lcs_ratio_attempted - lcs_ratio_rejected AS lcs_approved_surplus,
   wc_ratio_attempted - wc_ratio_rejected   AS wc_approved_surplus,
-  ed_ratio_attempted - ed_ratio_rejected   AS ed_approved_surplus
+  ed_ratio_attempted - ed_ratio_rejected   AS ed_approved_surplus,
+  ewc_ratio_attempted - ewc_ratio_rejected AS ewc_approved_surplus
 FROM score_buckets;
 `;
 
